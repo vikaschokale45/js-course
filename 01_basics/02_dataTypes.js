36c9258ef6813f1e1562439fb7e2dc88eb05f408
vikaschokale45 <vikascode352@gmail.com>
2026-07-01 17:42:34 +0000

    learned about dataTypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all JS code as newer version
+//alert("Hello") //we are using nodejs,not browser so it will not run
+
+let name ="Vikas"
+let age = 18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean =>true/false
+//null =>standalone value
+// undefined=>
+//symbol => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
