98ba7371419ede9e9db076d5f8430f4d1e16aebf
vikaschokale45 <vikascode352@gmail.com>
2026-07-01 18:31:53 +0000

    learned basic operations in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,25 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************** Operations **************//
+
+let value =3
+let negValue = -value
+// console.log(negValue)
+
+let str1 ="hello"
+let str2 =" Vikas"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
