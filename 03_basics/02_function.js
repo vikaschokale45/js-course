f40ce52af196bb045fdb6291dc806f63b6151c13
vikaschokale45 <vikascode352@gmail.com>
2026-07-05 10:50:31 +0000

    scope and global variable

---
+++ 03_basics/02_function.js
@@ -0,0 +1,37 @@
+function one(){
+    const username = "vikas"
+
+    function two(){
+        const website = "youtuber"
+        // console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "vikas"
+    if(username === "vikas"){
+       const website = " youtube" 
+    //    console.log(username+website)
+    }
+    // console.log(website)
+}
+
+// console.log(username)
+
+// +++++++++++++++++++++++//
+
+
+console.log(addone(5));
+function addone(num){
+    return num+1;
+}
+
+const addTwo = function(num){
+    return num+2;
+}
+console.log(addTwo(5))
+
