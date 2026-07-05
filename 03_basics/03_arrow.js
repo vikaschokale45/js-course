67515d11a8d6535c9b751b4fe59c4d2ada48a0d1
vikaschokale45 <vikascode352@gmail.com>
2026-07-05 11:15:48 +0000

    Arrow Functions

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,39 @@
+const user = {
+    username:"vikas",
+    price:999,
+
+    welcomeMessage:function(){
+        console.log(`${this.username} welcome to website`)
+        console.log(this)
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "rahul"
+// user.welcomeMessage()
+
+// console.log(this)
+
+// function chai(){
+//     let username ="vikas"
+//     console.log(this.username)
+// }
+// chai()
+
+const chai = () =>{
+    let username = "hitesh"
+    console.log(this)
+}
+
+// chai()
+
+// const addTwo = (num1,num2)=>{
+//     return num1+num2
+// }
+
+// const addTwo = (num1,num2)=> num1+num2;
+
+const addTwo = (num1,num2) => (num1+num2)
+
+console.log(addTwo(3,4))
