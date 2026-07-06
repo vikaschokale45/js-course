053850a6b747708225bebe1b419320d65f5ccea8
vikaschokale45 <vikascode352@gmail.com>
2026-07-06 15:10:38 +0000

    truthy and falsy value

---
+++ 04_control_flow/truhthy.js
@@ -0,0 +1,39 @@
+const userEmail = []
+if(userEmail){
+    console.log("Got user")
+} else{
+    console.log("Dont have user");
+}
+
+// falsy value
+
+// false :- 0,-0,BigInt 0n,"",null,undefined,Nan
+// truhthy :- true,[],"string","0","-0","false",{},function(){}
+
+// if (userEmail.length===0){
+//     console.log("Array is Empty")
+// }
+
+const emptyObj ={
+
+}
+if(Object.keys(emptyObj).length ===0){
+    console.log("Object is Empty")
+}
+
+//Nullish Coalescing Operator () 
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10 
+// console.log(val1)
+
+val1 = null??10??20
+console.log(val1)
+
+// Terniary Operator
+// condition ? true :false
+
+const iceTeaPrice =100
+iceTeaPrice >=80 ? console.log("more than 80") :  console.log("less than 80")
+
