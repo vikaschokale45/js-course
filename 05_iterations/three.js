753f765864487aab0f04761ab9b9e16cc6034057
vikaschokale45 <vikascode352@gmail.com>
2026-07-08 17:03:22 +0000

    map and for of

---
+++ 05_iterations/three.js
@@ -0,0 +1,34 @@
+// for of 
+
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+const greetings = "Hello World"
+// for(let greeting of greetings){
+//     console.log(greeting);
+// }
+
+// Maps
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"America")
+map.set('Fr',"France")
+map.set('IN',"India")
+// console.log(map)
+
+// for(let [key,value] of map){
+//     console.log(key,value)
+// }
+
+// const myObj = {
+//     'game1':'NPC',
+//     'game2':'BGMI'
+// }
+// for(let [key,value] of myObj){
+//     console.log(key,value);
+// }
+
+// for of cannot iterate object
