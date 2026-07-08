f98d9c510e9b9a6fbd99f76a53b0360b31f04e6e
vikaschokale45 <vikascode352@gmail.com>
2026-07-08 16:47:03 +0000

    while loop and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// While loop
+
+// let index = 0
+// while (index<10) {
+//     console.log(index)
+//     index = index +2;
+// }
+
+// let myArray = ["Naruto","Itachi","Sasuke"]
+// let index = 0
+// while(index < myArray.length){
+//     console.log(myArray[index]);
+//     index = index+1;
+// }
+
+// let score =11;
+// do { 
+//     console.log(`score is ${score}`)
+//     score++;
+// } while (score<=10);
+
