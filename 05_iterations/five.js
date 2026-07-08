91d13ff4617484cdff7fd93a85d284618fdb0534
vikaschokale45 <vikascode352@gmail.com>
2026-07-08 17:48:56 +0000

    for Each method

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+// for each loop
+const coding = ['js','ruby','java','python','cpp']
+
+// coding.forEach(function(item){
+//     console.log(item);
+// })
+
+// coding.forEach((item)=>{
+//     console.log(item)
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+// coding.forEach(printMe);
+
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr)
+// })
+
+const myCoding = [
+    {
+        languageName:'JavaScript',
+        languageFileName:'js'
+    },
+
+    {
+        languageName:"java",
+        languageFileName:'java'
+    },
+
+    {
+        languageName:'python',
+        languageFileName:'py'
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName)
+})
