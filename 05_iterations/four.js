53f60cb18afc22e8a40a27ede29e56350a4535dc
vikaschokale45 <vikascode352@gmail.com>
2026-07-08 17:23:54 +0000

    for in loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,17 @@
+// const myObj = {
+//     js:'JavaScript',
+//     cpp:'C++',
+//     rb:'ruby',
+//     swift:'swift by apple'
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is ofr ${myObj[key]}`);
+
+// }
+
+// const program = ["js","rb","py","java","cpp"]
+
+// for (const key in program) {
+//     console.log(program[key])
+// }
